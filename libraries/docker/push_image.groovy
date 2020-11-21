a1b13277667b66dcc00cafa995f69b8a8ab2620a
Souad Ali <c_souad.ali@smartdubai.ae>
2020-11-21 22:47:59 +0400

    add docker step

--- libraries/docker/push_image.groovy
+++ libraries/docker/push_image.groovy
@@ -1,9 +1,10 @@
-void call(){
+void call(docker){
     node{
     stage("Docker: push imae to DTR"){
 
        println "push image to DTR"
-       
+      docker.build registry + ":${docker.BUILD_NUMBER}"
+
     }
     }
 }
