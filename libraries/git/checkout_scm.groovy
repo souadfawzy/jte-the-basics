eb8d2b5e25b369db46366b462252525c6fd3c1ea
Souad Ali <c_souad.ali@smartdubai.ae>
2020-11-21 21:50:19 +0400

    update git

--- libraries/git/checkout_scm.groovy
+++ libraries/git/checkout_scm.groovy
@@ -4,30 +4,16 @@ void call(devbranch){
         println "source checkout ${devbranch.branch}"
         println "url ${devbranch.url}"
         checkout([
-        $class: 'GitSCM',
+        $class: 'GitSCM', 
+        branches: [[name: "${devbranch.branch}"]], 
         doGenerateSubmoduleConfigurations: false, 
-        branches: [[name: "${devbranch.branch}"]],
-        userRemoteConfigs: [[ url: "${devbranch.url}" ,credentialsId : "${devbranch.git_cred}"]]
-    ])
+        extensions: [[$class: 'SubmoduleOption', disableSubmodules: false, parentCredentials: false,recursiveSubmodules: true, reference: '', trackingSubmodules: false]], 
+        gitTool: 'Default',  
+        submoduleCfg: [], 
+        userRemoteConfigs: [[
+        credentialsId: "${devbranch.git_cred}", 
+        url: "${devbranch.url}"]]])
     }
-    }
+     }
 }
-
-// def call(){
-//     node{
-//         checkout(
-//             changelog:true,
-//             poll: true,
-//             scm: [
-//                 $class: 'GitSCM',
-//                 branches: [[name: "*/${config.branch}"]],
-//             doGenerateSubmoduleConfigurations: false,
-//             extensions: [
-//                 [$class: 'CheckoutOption', timeout: 10],
-//                 [$class: 'CloneOption', noTags: false, reference: '', timeout: 10]],
-//             submoduleCfg: [],
-//             userRemoteConfigs: [[url: config.url, credentialsId: config.cred]]]
-//         )
-//         sh(returnStdout: true, script: 'git rev-parse HEAD').trim()
-//     }
-// }
+        
--- libraries/maven/build.groovy
+++ libraries/maven/build.groovy
@@ -1,10 +1,10 @@
 void call(){
+node{
  stage("Maven: build scm"){
-    node{
+    
        println "build from the maven library"
        //sh "mvn clean verify"
        sh 'mvn -B -DskipTests clean package'
-       
     
     }
         }
