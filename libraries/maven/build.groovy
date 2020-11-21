8395800bdf50d0ea12cd7ad32cdaece45dd103d5
Souad Ali <c_souad.ali@smartdubai.ae>
2020-11-21 20:40:15 +0400

    add maven clean in maven step

--- libraries/maven/build.groovy
+++ libraries/maven/build.groovy
@@ -2,7 +2,7 @@ void call(){
  stage("Maven: build scm"){
     node{
        println "build from the maven library"
-        //sh "mvn clean verify"
+       sh "mvn clean verify"
     
     }
         }
