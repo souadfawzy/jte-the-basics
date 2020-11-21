d30455352c4709d9d1e7648e22c649181c1292b4
Souad Ali <c_souad.ali@smartdubai.ae>
2020-11-21 20:46:51 +0400

    mavn update

--- libraries/maven/build.groovy
+++ libraries/maven/build.groovy
@@ -2,7 +2,9 @@ void call(){
  stage("Maven: build scm"){
     node{
        println "build from the maven library"
-       sh "mvn clean verify"
+       //sh "mvn clean verify"
+       sh 'mvn -B -DskipTests clean package'
+       
     
     }
         }
