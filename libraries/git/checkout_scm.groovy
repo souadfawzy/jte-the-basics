42dbef17bc97bfa30342c30ef614d9b50ee11bae
Souad Ali <c_souad.ali@smartdubai.ae>
2020-11-21 13:21:35 +0400

    update checkout to only print url

--- libraries/git/checkout_scm.groovy
+++ libraries/git/checkout_scm.groovy
@@ -1,11 +1,3 @@
-void call(){
-        print scmConfig.branch
-    
-    checkout([
-        $class: 'GitSCM'
-        //doGenerateSubmoduleConfigurations: false, 
-        branches: [[name:  scmConfig.branch ]],
-        userRemoteConfigs: [[ url: scmConfig.url ,credentialsId : scmCheckout.credentialsId]]
-    ])
-
+void call(url){
+        print url
 }
